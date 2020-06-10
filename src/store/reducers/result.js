15175d75117ccde0f57c236a867543096b08999b
ashutoshtiwari3309 <ashutosh.tiwari@squareboat.com>
2020-06-10 22:13:21 +0530

    Seperating Filter, concat function in Reducer, Helper function

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -5,21 +5,31 @@ const initialState = {
 	result: []
 };
 
+const filterArray = (state, action) => {
+	return updateObject(state, {
+		result: state.result.filter(res => {
+			return res.id !== action.payload.value;
+		})
+	});
+};
+
+const concatToArray = (state, action) => {
+	return updateObject(state, {
+		result: state.result.concat({
+			id: new Date(),
+			value: action.payload.value
+		})
+	});
+};
+
 const reducer = (state = initialState, action) => {
 	switch (action.type) {
 		case actionTypes.SAVE_COUNTER:
-			return updateObject(state, {
-				result: state.result.concat({
-					id: new Date(),
-					value: action.payload.value
-				})
-			});
+			return concatToArray(state, action);
+
 		case actionTypes.DELETE_ELEMENT:
-			return updateObject(state, {
-				result: state.result.filter(res => {
-					return res.id !== action.payload.value;
-				})
-			});
+			return filterArray(state, action);
+
 		default:
 			return state;
 	}
